de017c25b981b24f737eb3f3003e14da9efe9a0f
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-10-28 11:54:13 +0500

    [Write some comments about data types in C++]

---
+++ Data Types In C++/dataTypes.cpp
@@ -0,0 +1,66 @@
+/*
+    In any programming language, we have some special
+    data types to tell the computer memory regarding
+    what type of data we store in a specific variable in
+    the memory.
+
+    C++ supports 3 different data types to store different
+    data in the computer memory.
+
+    1) Primitive data type or Built-in data type or Fundamental data types
+    2) Derived data types
+    3) User-defined data types.
+
+*/
+
+/*
+
+********** Primitive Data Types **********
+
+    These are the built-in or pre-defined data types provided
+    by the C++ programming language. Following are the different types
+    of it:
+
+    . Integer
+    . Character
+    . Boolean
+    . Floating Point
+    . Double Floating Point
+    . Valueless or Void
+    . Wide character
+
+*/
+
+/*
+
+********** Derived Data Types **********
+
+    These are the data types that are created or derived from fundamental
+    data types. These are formed by combining other data types in various
+    ways, such as arrays, structures, unions, enums, classes, and pointers.
+
+    . Function
+    . Array
+    . Pointer
+    . Reference
+
+*/
+
+/*
+
+********** User-defined Data Types **********
+
+    Based on programmer needs, these data types create by combining fundamental
+    or derived data types provided by the language.
+
+    In C++ user-defined data types mainly includes:
+
+    . Structures
+    . Classes
+    . Enums
+    . Unions
+    . Typedefs
+
+*/
+
+
